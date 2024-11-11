237a8267b5b2159eba17344ad64d0762b7c32226
lsgordon <lsgordon@haverford.edu>
2024-11-11 10:50:56 -0500

    Updates on data

--- HMM.py
+++ HMM.py
@@ -2,9 +2,30 @@
 Hidden Markov Model class
 This file contains the source code for the hidden markov model for this assignment
 """
-
 import numpy as np
-
+import pandas as pd
+np.random.seed(42)
 class HiddenMarkovModel:
-    def __init__(self) -> None:
+    def __init__(self,observation_sequence,number_states) -> None:
+
+        # first step of the model is to initialize all of the parameters to random states
+        transition_matrix = np.random.rand(number_states,number_states)
+        transition_matrix = transition_matrix / transition_matrix.sum(axis=0, keepdims=True)
+
+        # conditional probabilities of seeing some value given state
+        emission_matrix = np.random.rand(number_states,len(np.unique(observation_sequence)))
+        emission_matrix = emission_matrix / emission_matrix.sum(axis=1, keepdims=True)
+
+        # pi distribution
+        pi = np.random.rand(number_states,1)
+        pi = pi / pi.sum()
+
+        print(emission_matrix,transition_matrix)
+        # random initial state sequence
+
         pass
+    
+
+if __name__ == "__main__":
+    df = pd.read_csv('Data/input.csv')
+    model = HiddenMarkovModel(df['decile'],8)
--- lab_notebook.md
+++ lab_notebook.md
@@ -1,1 +1,12 @@
+<<<<<<< HEAD
 Initial Commit
+=======
+# Lab notebook
+## 11/9/24 2:00 pm
+- setting up the github repository
+- making the first starter files for the project
+## 11/10/24 12:00 pm
+- putting some stuff in the init method
+- wrangling data because I can't actually start developing the class until I have some sort of df
+- found decile of log returns
+>>>>>>> 86cfb90 (Updates on data)
